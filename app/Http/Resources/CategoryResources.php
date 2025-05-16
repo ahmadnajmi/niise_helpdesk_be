64bbabde76004d9db44702a8cf20e2eed217190b
DESKTOP-G601CFI\najmi <najmiemon4223@gmail.com>
2025-05-16 15:55:30 +0800

    update category resource

--- app/Http/Resources/CategoryResources.php
+++ app/Http/Resources/CategoryResources.php
@@ -22,6 +22,7 @@ class CategoryResources extends JsonResource
             'description' => $this->description,
             'is_active' => $this->is_active,
             'category_id' => $this->category_id,
+            'category_main_details' => $this->mainCategory ? new CategoryResources($this->mainCategory) : null,
             'created_by' => $this->createdBy->name .' - '. $this->createdBy->email ,
             'updated_by' => $this->updatedBy->name .' - '. $this->updatedBy->email ,
             'created_at' => $this->created_at->format('d-m-Y'),
