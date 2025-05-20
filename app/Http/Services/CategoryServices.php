15709128d42d269bd398a8375502fbe1e7245254
DESKTOP-G601CFI\najmi <najmiemon4223@gmail.com>
2025-05-20 15:08:49 +0800

    update only if have category main

--- app/Http/Services/CategoryServices.php
+++ app/Http/Services/CategoryServices.php
@@ -16,7 +16,9 @@ class CategoryServices
 
     public static function update(Category $category,$data){
 
-        $data = self::processData($data);
+        if(isset($data['category_id'])){
+            $data = self::processData($data);
+        }
 
         $create = $category->update($data);
 
