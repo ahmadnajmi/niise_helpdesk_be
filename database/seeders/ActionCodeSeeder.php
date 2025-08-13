c3e60e475bfcc955530325f9e4c1ae2b44867d22
DESKTOP-BM66RJO\User <najmiemon4223@gmail.com>
2025-08-13 19:59:59 +0800

    update seeder

--- database/seeders/ActionCodeSeeder.php
+++ database/seeders/ActionCodeSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\ActionCode;
+use Illuminate\Support\Facades\DB;
 
 class ActionCodeSeeder extends Seeder
 {
@@ -13,21 +14,51 @@ class ActionCodeSeeder extends Seeder
      */
     public function run(): void
     {
-       DB::table('ACTION_CODES')->truncate();
+        DB::table('ACTION_CODES')->truncate();
 
         DB::statement("ALTER SEQUENCE ACTION_CODES_ID_SEQ RESTART START WITH 1");
 
-        $categorys = ['MOBILE'];
+        $action_codes = [
+            [   
+                "name" => "Penyaluran Insiden",
+                "nickname" => "ESCL",
+                'description' => 'Menyalurkan insiden kepada juruteknik atau kumpulan yang berkaitan',
+                'send_email' => 1,
+                'email_recipient_id' => 2,
+            ],
+            [   
+                "name" => "Penyelesaian Sebenar",
+                "nickname" => "ACTR",
+                'description' => 'Log telah diselesaikan',
+                'send_email' => 1,
+                'email_recipient_id' => 2,
+            ],
+            [   
+                "name" => "Tutup",
+                "nickname" => "CLSD",
+                'description' => 'Log insiden ditutup',
+                'send_email' => 1,
+                'email_recipient_id' => 1,
+            ],
+            [   
+                "name" => "Log insiden bermula",
+                "nickname" => "INIT",
+                'description' => 'Menunjukkan log insiden bermula',
+                'send_email' => 0,
+            ],
+            [   
+                "name" => "Kemaskini",
+                "nickname" => "UPDT",
+                'description' => 'Kemaskini status terkini',
+                'send_email' => 0,
+            ],
+        ];
 
 
-        foreach($categorys as $category_code){
+        foreach($action_codes as $action_code){
 
-            $data['name'] = $category_code;
-            $data['level'] = 1;
-            $data['code'] = '01';
-            $data['description'] = $category_code;
 
-            $create = ActionCode::create($data);
+            $create = ActionCode::create($action_code);
 
         }
     }
