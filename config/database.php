b66d0e0b858374a5970eed6960cd282a4eb64b7d
DESKTOP-BM66RJO\User <najmiemon4223@gmail.com>
2025-09-02 21:13:08 +0800

    update config

--- config/database.php
+++ config/database.php
@@ -113,7 +113,7 @@ return [
         ],
 
         'oracle' => [
-            'driver' =>'oracle',
+            'driver' =>'oci8',
             'host' => env('DB_HOST'),
             'port' => env('DB_PORT'),
             'database' => env('DB_DATABASE'),
