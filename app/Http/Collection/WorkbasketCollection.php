038001ae79fb9f0d217724680bed6b824c5ea462
DESKTOP-BM66RJO\User <najmiemon4223@gmail.com>
2025-09-03 12:02:09 +0800

    update workbasket

--- app/Http/Collection/WorkbasketCollection.php
+++ app/Http/Collection/WorkbasketCollection.php
@@ -3,6 +3,10 @@
 namespace App\Http\Collection;
 
 use Illuminate\Http\Request;
+use App\Http\Resources\CategoryResources;
+use App\Http\Resources\SlaTemplateResources;
+use App\Http\Resources\GroupResources;
+
 
 class WorkbasketCollection extends BaseResource
 {
@@ -16,10 +20,15 @@ class WorkbasketCollection extends BaseResource
         return $this->collection->transform(function ($query) use($request){
             $return =  [
                 'id' => $query->id,
-                'date' => $query->date,
                 'incident_id' => $query->incident_id,
+                'date' => $query->date->format('d-m-Y H:i:s'),
+                'category_details' => $query->incident?->categoryDescription ? new CategoryResources($query->incident->categoryDescription) : null,
+                'sla_template_details' => $query->incident?->sla?->slaTemplate ? new SlaTemplateResources($query->incident->sla->slaTemplate) : null,
+                'information' => $query->incident?->information,
+                'group_details' =>  $query->incident?->group ? new GroupResources($query->incident->group) : null,
                 'handle_by' => $query->handle_by,
                 'status' => $query->status,
+                'status_desc' => $query->statusDesc?->name,
                 'created_at' => $query->created_at->format('d-m-Y'),
                 'updated_at' => $query->updated_at->format('d-m-Y'),
             ];
--- app/Models/Workbasket.php
+++ app/Models/Workbasket.php
@@ -17,12 +17,20 @@ class Workbasket extends BaseModel
         'status',
     ];
 
+    protected $casts = [
+        'date' => 'datetime:Y-m-d',
+    ];
+
     const NEW = 1;
     const IN_PROGRESS = 2;
     const OPENED = 3;
 
-    public function user(){
-        return $this->belongsTo(User::class, 'handle_by');
+    public function incident(){
+        return $this->belongsTo(Incident::class, 'incident_id');
+    }
+
+     public function statusDesc(){
+        return $this->hasOne(RefTable::class,'ref_code','status')->where('code_category', 'workbasket_status');
     }
     
 }
