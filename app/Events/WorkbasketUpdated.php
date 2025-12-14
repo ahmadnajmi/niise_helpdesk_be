08570e1d7bf5664d68d8f8957360ac9c4a39746f
ahmad najmi <najmiemon4223@gmail.com>
2025-12-14 10:56:27 +0700

    change events

--- app/Events/WorkbasketUpdated.php
+++ app/Events/WorkbasketUpdated.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 
-class WorkbasketUpdated implements ShouldBroadcastNow
+class WorkbasketUpdated implements ShouldBroadcast
 {
     public $incident;
     public $trigger;
