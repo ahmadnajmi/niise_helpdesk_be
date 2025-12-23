8c1f96bb21eac140fb53e93bfb62440cd7929fd8
ahmad najmi <najmiemon4223@gmail.com>
2025-12-23 19:29:36 +0800

    update email and action code email

--- resources/views/mail/action_code.blade.php
+++ resources/views/mail/action_code.blade.php
@@ -1,69 +1,3 @@
-{{-- <x-mail::message>
-<br>
-<br>
-<table>
-  <tr>
-    <td style="white-space: nowrap;">Customer</td>
-    <td style="white-space: nowrap;">:</td>
-    <td style="white-space: nowrap;">{{ $incident->complaint?->name }}</td>
-  </tr>
-  <tr>
-    <td style="white-space: nowrap;">Location</td>
-    <td style="white-space: nowrap;">:</td>
-
-    <td style="white-space: nowrap;">{{ $incident->complaint?->address }}</td>
-  </tr>
-  <tr>
-    <td style="white-space: nowrap;">Contact Person</td>
-    <td style="white-space: nowrap;">:</td>
-
-    <td style="white-space: nowrap;">{{ $incident->complaint?->name }}</td>
-  </tr>
-  <tr>
-    <td style="white-space: nowrap;">Hp Number</td>
-    <td style="white-space: nowrap;">:</td>
-
-    <td style="white-space: nowrap;">{{ $incident->complaint?->phone_no }}</td>
-  </tr>
-  <tr>
-    <td style="white-space: nowrap;">Office Number</td>
-    <td style="white-space: nowrap;">:</td>
-
-    <td style="white-space: nowrap;">{{ $incident->complaint?->office_phone_no }}</td>
-  </tr>
-  <tr>
-    <td style="white-space: nowrap;">Problem Desc.</td>
-    <td style="white-space: nowrap;">:</td>
-
-    <td>{{ $incident->information }}</td>
-  </tr>
-</table>
-<br>
-<br>
-<br>
-<br>
-<hr style="border: none; border-bottom: 1px dotted #000;"/>
-<br>
-<br>
-<table>
-  <tr>
-    <td>Resolution</td>
-    <td colspan="2">:</td>
-  </tr>
-  @foreach($incident->incidentResolution as $resolution)
-  <tr>
-    <td>{{ $resolution->created_at?->format('d/M/Y H:i:s') }}</td>
-    <td>:</td>
-
-    <td>{{ $resolution->notes }}</td>
-  </tr>
-  @endforeach
-</table>
-
-<br>
-<br>
-{{ $email_template->notes }}
-</x-mail::message> --}}
 <x-mail::message>
 # Laporan Insiden
 
@@ -122,6 +56,6 @@
 @endforeach --}}
 <br>
 <br>
-{{ $email_template->notes }}
+{{ $email_template?->notes }}
 
 </x-mail::message>
