a4ea6a7791c11397e17e41d0d93b166f9dd53844
ahmad najmi <najmiemon4223@gmail.com>
2025-12-29 13:40:34 +0800

    update email forget password

--- resources/views/mail/forget_password.blade.php
+++ resources/views/mail/forget_password.blade.php
@@ -3,9 +3,15 @@
 Dimaklumkan bahawa kata laluan anda telah diset semula.
 Untuk akses seterusnya, sila gunakan kata laluan sementara seperti berikut:
 
-6 huruf pertama nama @ 6 nombor terakhir kad pengenalan.
-contoh (suhani@016010)
+6 huruf pertama nama (HURUF BESAR) dan 6 nombor terakhir kad pengenalan.
+contoh
+
+Nama : Ahmad Najmi bin Sidek
+Kad Pengenalan : 951491-43-9173
+Kata Laluan yang Baru : AHMADN439173
 
 Sila tukar kata laluan selepas log masuk bagi tujuan keselamatan.
+
+Sekian,
 
 </x-mail::message>
