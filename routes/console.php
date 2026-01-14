4b0eac5efcdc9d56d24d847a73dac730600ac8c3
ahmad najmi <najmiemon4223@gmail.com>
2026-01-14 19:32:35 +0800

    update scheduler

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::command('read_emails_incident')->everyFiveMinutes();
+Schedule::command('holidays', [date('Y')])->dailyAt('18:50');
